f6268d3347cf33dfa3325299237740a8da6fecad
Jonathan Buchholz <63243849+JonathanBuchh@users.noreply.github.com>
2021-08-17 15:38:27 -0400

    Complete List-1 problems

--- Python/List-1.py
+++ Python/List-1.py
@@ -41,3 +41,33 @@ def rotate_left3(nums):
 
 # reverse3
 def reverse3(nums):
+    output = [nums[2], nums[1], nums[0]]
+    return output
+
+# max_end3
+def max_end3(nums):
+    larger = max(nums[0], nums[2])
+    return [larger, larger, larger]
+
+# sum2
+def sum2(nums):
+    if len(nums) == 0:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    if len(nums) > 1:
+        return nums[0] + nums[1]
+
+# middle_way
+def middle_way(a, b):
+    return [a[1], b[1]]
+
+# make_ends
+def make_ends(nums):
+    return [nums[0], nums[len(nums)-1]]
+
+# has23
+def has23(nums):
+    if nums[0] == 2 or nums[0] == 3 or nums[1] == 2 or nums[1] == 3:
+        return True
+    return False
